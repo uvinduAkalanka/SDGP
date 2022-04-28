8a04fb3da2ef9e36083a31461422f1bc8a5bf664
LAPTOP-RA29OSSR\Ruwanji Amarasinghe <ruwanjiamarasinghe@gmail.com>
2022-04-29 01:34:33 +0530

    navigation bar/ menue for the app is added

---
+++ navbar.dart
@@ -0,0 +1,85 @@
+import 'package:demo/aboutUs.dart';
+import 'package:demo/home.dart';
+import 'package:demo/search.dart';
+import 'package:demo/signin.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class navbar extends StatelessWidget {
+  const navbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            buildHeader(context),
+            buildMenuItems(context),
+          ],
+        ),
+      ), 
+    );
+  }
+
+  Widget buildHeader(BuildContext context){
+    return Container(
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).padding.top,
+      ),
+    );
+  }
+
+
+  Widget buildMenuItems(BuildContext context){
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () {
+          Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const HomePage()),);},
+            child: const ListTile(
+              leading: Icon(Icons.home_outlined),
+              title: Text('Search'),
+            ),
+          ),
+
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const signIn()),);},
+          child: const ListTile(
+            leading: Icon(Icons.login),
+            title: Text('Login'),
+          ),
+        ),
+
+
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const aboutUs()),);},
+          child: const ListTile(
+            leading: Icon(Icons.info),
+            title: Text('About Us'),
+          ),
+        ),
+
+        GestureDetector(
+          onTap: () {
+            FirebaseAuth.instance.signOut().then((value) {
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const Home()),
+              );
+            });
+            },
+          child: const ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Logout'),
+          ),
+        ),
+      ],
+    );
+  }
+}
